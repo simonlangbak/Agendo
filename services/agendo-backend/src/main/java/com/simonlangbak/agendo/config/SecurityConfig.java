dfab0f01f11167854fdaee26f1ddafbec2c79997
Simon Langbak <simon_langbak@live.dk>
2024-12-30 22:16:29 +0100

    Added docker compose

--- services/agendo-backend/src/main/java/com/simonlangbak/agendo/config/SecurityConfig.java
+++ services/agendo-backend/src/main/java/com/simonlangbak/agendo/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
